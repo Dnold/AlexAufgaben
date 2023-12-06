160fbe8c1628136baccdafc26caf4ebbdacf515d
Dnold <dnold.png@gmail.com>
2023-12-06 15:38:34 +0100

    Add project files.

---
+++ AlexAufgaben/Aufgaben.cs
@@ -0,0 +1,123 @@
+﻿using System;
+/*
+    //DatenTypen
+    int ganzeZahl = 5;
+    double kommaZahl = 5.5;
+    string text = "Hallo Welt";
+    bool wahrheitswert = true;
+    char zeichen = 'a';
+    float kommaZahl2 = 5.5f;
+
+     //Ausgabe
+    Console.WriteLine("Hallo Welt");  --> Hallo Welt mit Zeilenumbruch
+    Console.Write("Hallo Welt"); --> Hallo Welt ohne Zeilenumbruch
+    Console.WriteLine("Hallo Welt" + "Hallo Welt"); --> Hallo WeltHallo Welt (Kombination von Strings)
+    Console.WriteLine("Hallo Welt" + 5); --> Hallo Welt5 (Kombination von String und Zahl)
+
+
+     //Eingabe
+    Console.ReadLine(); --> Wartet auf Eingabe und speichert diese in einer Variable
+
+    string text = Console.ReadLine(); --> Speichert die Eingabe in der Variable text
+
+    int zahl = Convert.ToInt32(Console.ReadLine()); --> Wandelt die Eingabe in eine Zahl um
+
+     //Rechnen
+    int zahl1 = 5;
+    int zahl2 = 10;
+
+    int ergebnis = zahl1 + zahl2; --> 15 (Addition)
+    int ergebnis = zahl1 - zahl2; --> -5 (Subtraktion)
+    int ergebnis = zahl1 * zahl2; --> 50 (Multiplikation)
+    int ergebnis = zahl1 / zahl2; --> 0 (Division)
+    int ergebnis = zahl1 % zahl2; --> 5 (Modulo)
+
+
+     //Bedingungen
+    if(zahl1 == zahl2) --> Prüft ob zahl1 gleich zahl2 ist
+    if(zahl1 != zahl2) --> Prüft ob zahl1 ungleich zahl2 ist
+    if(zahl1 > zahl2) --> Prüft ob zahl1 größer zahl2 ist
+    if(zahl1 < zahl2) --> Prüft ob zahl1 kleiner zahl2 ist
+
+
+     //Schleifen
+    for(int i = 0; i < 10; i++) --> Zählt von 0 bis 9
+    for(int i = 10; i > 0; i--) --> Zählt von 10 bis 1
+    for(int i = 0; i < 10; i+=2) --> Zählt von 0 bis 9 in 2er Schritten
+
+    while(zahl1 < zahl2) --> Wiederholt den Code solange zahl1 kleiner zahl2 ist
+
+    do{ }while(zahl1 < zahl2) --> Wiederholt den Code solange zahl1 kleiner zahl2 ist, mindestens einmal
+
+    foreach(int zahl in zahlen) --> Wiederholt den Code für jedes Element in zahlen
+
+    //Methoden
+    public static void MeineMethode() --> Methode ohne Rückgabewert
+    public static int MeineMethode() --> Methode mit Rückgabewert
+    public static int MeineMethode(int zahl) --> Methode mit Rückgabewert und Parameter
+    public static void MeineMethode(int zahl) --> Methode ohne Rückgabewert und mit Parameter
+    public static void MeineMethode(int zahl, string text) --> Methode ohne Rückgabewert und mit mehreren Parametern
+
+    //Nutzen von Methoden
+    MeineMethode(); --> Aufruf der Methode
+    int ergebnis = MeineMethode(); --> Aufruf der Methode und speichern des Rückgabewertes in der Variable ergebnis
+    MeineMethode(5); --> Aufruf der Methode mit Parameter
+    MeineMethode(5, "Hallo Welt"); --> Aufruf der Methode mit mehreren Parametern
+     */
+
+
+
+//Achte bei den Aufgaben darauf, dass die Methoden einen "Returntype" erwarten. Wenn du glaubst du hast den richtigen und gefragten Wert in einer Variabel
+//gespeichert, Gib sie mit "return {variabelname} zurück. Den Rückgabe Typen findest du in der Deklarierung der Methode
+//Beispiel:
+//          RückgabeTyp     Parameter
+//               |              |
+//public static int Aufgabe1(int n){
+//
+//Also kannt du hier auf den Parameter int n zugreifen, der gegeben ist und
+//Du musst einen int mit return {variabelname für ein int}; zurückgegeben.
+class Aufgaben
+{
+    //Schreibe ein Program, dass den eingebenen Parameter 'n' um +1 erhöht und dann wieder ausgibt
+    public static int Aufgabe1(int n)
+    {
+        return 0;
+    }
+    //Schreibe ein Programm, das die beiden eingegebenen Paramater 'a' und 'b' addiert und die summe dann wieder ausgibt
+    public static int Aufgabe2(int a, int b)
+    {
+        return 0;
+    }
+    //Schreibe ein Programm, dass alle int Zahlenwerte in dem eingegebenen int array parameter zusammen addiert und dann die summe wieder ausgibt
+    public static int Aufgabe3(int[] a)
+    {
+        return 0;
+    }
+    public static bool Aufgabe4(int zahl)
+    {
+        bool wahrheitswert = false;
+        return wahrheitswert; //Gib hier den Wahrheitswert zurück
+    }
+    //Aufgabe 5
+    //Schreibe ein Programm, welches einen Wahrheitswert zurückgibt, der wahr ist wenn die Zahl größer als 10 ist und kleiner als 20.
+    //Benutze dafür nur ein if statement
+    public static bool Aufgabe5(int zahl)
+    {
+        bool wahrheitswert = false;
+        return wahrheitswert; //Gib hier den Wahrheitswert zurück
+    }
+    //Aufgabe 6
+    //Schreibe ein Programm, welches einen Wahrheitswert zurückgibt,der wahr ist wenn in dem String Array das Wort "Hurensohn" enthalten ist.
+    public static bool Aufgabe6(string[] array)
+    {
+        bool wahrheitswert = false;
+        return wahrheitswert; //Gib hier den Wahrheitswert zurück
+    }
+    //Aufgabe 7
+    //Schreibe ein Programm, welches die Quersumme der gegeben Zahl zurückgibt
+    public static int Aufgabe7(int zahl)
+    {
+        return 0;
+    }
+}
+
---
+++ AlexAufgaben/Program.cs
@@ -0,0 +1,346 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+public class Tester
+{
+    //A method Tester that expects the given input and the expected output to then check if the Method is doing its Job
+
+    public static void TestIntBool(IntBoolTest intBool)
+    {
+        string prefix = "";
+        if (intBool.hidden)
+        {
+            prefix = "Hidden ";
+        }
+        if (intBool.output == intBool.expectedOutput)
+        {
+            Console.WriteLine(prefix + "Test passed ;)", Color.Green);
+        }
+        else
+        {
+            Console.WriteLine(prefix + "Test failed :/", Color.Red);
+        }
+        if (intBool.hidden)
+        {
+            Console.WriteLine("Hidden Test");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Input: " + intBool.input);
+            Console.WriteLine("Output: " + intBool.output);
+            Console.WriteLine("Expected Output: " + intBool.expectedOutput);
+        }
+        Console.WriteLine();
+        Console.WriteLine("<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//");
+        Console.WriteLine();
+    }
+
+    public static void TestStringArrayBool(StringArrayBoolTest test)
+    {
+        string prefix = "";
+        if (test.hidden)
+        {
+            prefix = "Hidden ";
+        }
+        if (test.output == test.expectedOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(prefix + "Test passed ;)", Color.Green);
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(prefix + "Test failed :/", Color.Red);
+            Console.ResetColor();
+        }
+
+
+        if (!test.hidden)
+        {
+            Console.WriteLine("Input: ");
+            foreach (string i in test.input)
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.WriteLine("Output: " + test.output);
+            Console.WriteLine("Expected Output: " + test.expectedOutput);
+        }
+        else
+        {
+            Console.WriteLine("Hidden Test");
+        }
+    }
+    public static void TestIntInIntOut(IntInIntOutTest intIn)
+    {
+        string prefix = "";
+        if (intIn.hidden)
+        {
+            prefix = "Hidden ";
+        }
+        if (intIn.output == intIn.expectedOutput)
+        {
+            Console.WriteLine(prefix + "Test passed ;)", Color.Green);
+        }
+        else
+        {
+            Console.WriteLine(prefix + "Test failed :/", Color.Red);
+        }
+        if (intIn.hidden)
+        {
+            Console.WriteLine("Hidden Test");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Input: " + intIn.input);
+            Console.WriteLine("Output: " + intIn.output);
+            Console.WriteLine("Expected Output: " + intIn.expectedOutput);
+        }
+        Console.WriteLine();
+        Console.WriteLine("<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//");
+        Console.WriteLine();
+    }
+    public static void TestIntIntInIntOut(IntIntInIntOutTest intintIn)
+    {
+        string prefix = "";
+        if (intintIn.hidden)
+        {
+            prefix = "Hidden ";
+        }
+        if (intintIn.output == intintIn.expectedOutput)
+        {
+            Console.WriteLine(prefix + "Test passed ;)", Color.Green);
+        }
+        else
+        {
+            Console.WriteLine(prefix + "Test failed :/", Color.Red);
+        }
+        if (intintIn.hidden)
+        {
+            Console.WriteLine("Hidden Test");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Input1: " + intintIn.input1);
+            Console.WriteLine("Input2: " + intintIn.input2);
+            Console.WriteLine("Output: " + intintIn.output);
+            Console.WriteLine("Expected Output: " + intintIn.expectedOutput);
+        }
+        Console.WriteLine();
+        Console.WriteLine("<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//");
+        Console.WriteLine();
+    }
+    public static void TestIntArrayInt(IntArrayIntTest intArray)
+    {
+        string prefix = "";
+        if (intArray.hidden)
+        {
+            prefix = "Hidden ";
+        }
+        if (intArray.output == intArray.expectedOutput)
+        {
+            Console.WriteLine(prefix + "Test passed ;)", Color.Green);
+        }
+        else
+        {
+            Console.WriteLine(prefix + "Test failed :/", Color.Red);
+        }
+        if (intArray.hidden)
+        {
+            Console.WriteLine("Hidden Test");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Input: ");
+            foreach (int i in intArray.input)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Output: " + intArray.output);
+            Console.WriteLine("Expected Output: " + intArray.expectedOutput);
+        }
+        Console.WriteLine();
+        Console.WriteLine("<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//<>//");
+        Console.WriteLine();
+    }
+}
+public class IntBoolTest
+{
+    public int input;
+    public bool output;
+    public bool expectedOutput;
+    public bool hidden = false;
+}
+public class IntInIntOutTest
+{
+    public int input;
+    public int output;
+    public int expectedOutput;
+    public bool hidden = false;
+}
+public class IntIntInIntOutTest
+{
+    public int input1;
+    public int input2;
+    public int output;
+    public int expectedOutput;
+    public bool hidden = false;
+}
+public class StringArrayBoolTest
+{
+    public string[] input = new string[0];
+    public bool output;
+    public bool expectedOutput;
+    public bool hidden = false;
+}
+public class IntArrayIntTest
+{
+    public int[] input = new int[0];
+    public int output;
+    public int expectedOutput;
+    public bool hidden = false;
+}
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welche Aufgabe willst du Testen? (1-6)");
+        string input = Console.ReadLine();
+        Console.Clear();
+        switch (input)
+        {
+            case "1":
+                Aufgabe1Test();
+                break;
+            case "2":
+                Aufgabe2Test();
+                break;
+            case "3":
+                Aufgabe3Test();
+                break;
+            case "4":
+                Aufgabe4Test();
+                break;
+            case "5":
+                Aufgabe5Test();
+                break;
+            case "6":
+                Aufgabe6Test();
+                break;
+            default:
+                Console.WriteLine("Falsche Eingabe");
+                break;
+        }
+        Console.WriteLine("Drücke Enter um einen anderen Test zu starten");
+        Console.ReadLine();
+        Console.Clear(); 
+        Main(null);
+
+    }
+    public static void Aufgabe1Test()
+    {
+        IntInIntOutTest[] AufgabeEinsTests =
+        {
+            new IntInIntOutTest { input = 1, output = Aufgaben.Aufgabe1(1), expectedOutput = 2, hidden = false },
+            new IntInIntOutTest { input = 4, output = Aufgaben.Aufgabe1(4), expectedOutput = 4, hidden = false },
+            new IntInIntOutTest { input = -1, output = Aufgaben.Aufgabe1(-1), expectedOutput = 0, hidden = true },
+            new IntInIntOutTest { input = 21, output = Aufgaben.Aufgabe1(21), expectedOutput = 22, hidden = false },
+            new IntInIntOutTest { input = 420, output = Aufgaben.Aufgabe1(420), expectedOutput = 421, hidden = false }
+        };
+        foreach (IntInIntOutTest item in AufgabeEinsTests)
+        {
+            Tester.TestIntInIntOut(item);
+        }
+    }
+    public static void Aufgabe2Test()
+    {
+        IntIntInIntOutTest[] AufgabeZweiTest =
+        {
+            new IntIntInIntOutTest { input1 = 1, input2 = 2, output = Aufgaben.Aufgabe2(1,2), expectedOutput = 3, hidden = false },
+            new IntIntInIntOutTest { input1 = 4, input2 = 4, output = Aufgaben.Aufgabe2(4,4), expectedOutput = 8, hidden = false },
+            new IntIntInIntOutTest { input1 = -1, input2 = 1, output = Aufgaben.Aufgabe2(-1,1), expectedOutput = 0, hidden = true },
+            new IntIntInIntOutTest { input1 = 21, input2 = 21, output = Aufgaben.Aufgabe2(21,21), expectedOutput = 42, hidden = false },
+            new IntIntInIntOutTest { input1 = 420, input2 = 420, output = Aufgaben.Aufgabe2(420,420), expectedOutput = 840, hidden = false },
+            new IntIntInIntOutTest { input1 = 9, input2 = 10, output = Aufgaben.Aufgabe2(9,10), expectedOutput = 19, hidden = false }
+        };
+    }
+    public static void Aufgabe3Test()
+    {
+        IntArrayIntTest[] Aufgabe3Tests =
+        {
+            new IntArrayIntTest { input = new int[]{1,2,3,4,5}, output = Aufgaben.Aufgabe3(new int[]{1,2,3,4,5}), expectedOutput = 15, hidden = false },
+            new IntArrayIntTest { input = new int[]{4,4,4,4,4}, output = Aufgaben.Aufgabe3(new int[]{4,4,4,4,4}), expectedOutput = 20, hidden = false },
+            new IntArrayIntTest { input = new int[]{-1,1,1,1,1}, output = Aufgaben.Aufgabe3(new int[]{-1,1,1,1,1}), expectedOutput = 3, hidden = true },
+            new IntArrayIntTest { input = new int[]{21,21,21,21,21}, output = Aufgaben.Aufgabe3(new int[]{21,21,21,21,21}), expectedOutput = 105, hidden = false },
+            new IntArrayIntTest { input = new int[]{420,420,420,420,420}, output = Aufgaben.Aufgabe3(new int[]{420,420,420,420,420}), expectedOutput = 2100, hidden = false },
+            new IntArrayIntTest { input = new int[]{9,10,11,12,13}, output = Aufgaben.Aufgabe3(new int[]{9,10,11,12,13}), expectedOutput = 55, hidden = false }
+        };
+        foreach(var item in Aufgabe3Tests)
+        {
+            Tester.TestIntArrayInt(item);
+        }
+    }
+    public static void Aufgabe4Test()
+    {
+        IntBoolTest[] AufgabeVierTests = {
+                   new IntBoolTest { input = 5, output = Aufgaben.Aufgabe4(5), expectedOutput = false},
+                   new IntBoolTest { input = 10, output = Aufgaben.Aufgabe4(10), expectedOutput = false,hidden = true },
+                   new IntBoolTest { input = 11, output = Aufgaben.Aufgabe4(11), expectedOutput = true,hidden = true },
+                   new IntBoolTest { input = 20, output = Aufgaben.Aufgabe4(20), expectedOutput = true },
+                   new IntBoolTest { input = 21, output = Aufgaben.Aufgabe4(21), expectedOutput = true },
+               };
+        foreach (IntBoolTest item in AufgabeVierTests)
+        {
+            Tester.TestIntBool(item);
+        }
+    }
+    public static void Aufgabe5Test()
+    {
+        IntBoolTest[] AufgabeFünfTests = {
+                   new IntBoolTest { input = 7, output =  Aufgaben.Aufgabe5(5), expectedOutput = false,hidden = true },
+                   new IntBoolTest { input = 11, output = Aufgaben.Aufgabe5(10), expectedOutput = false },
+                   new IntBoolTest { input = 10, output = Aufgaben.Aufgabe5(11), expectedOutput = true },
+                   new IntBoolTest { input = 20, output = Aufgaben.Aufgabe5(20), expectedOutput = false,hidden = true },
+                   new IntBoolTest { input = 24, output = Aufgaben.Aufgabe5(21), expectedOutput = false , hidden = true},
+               };
+        foreach (IntBoolTest item in AufgabeFünfTests)
+        {
+            Tester.TestIntBool(item);
+        }
+    }
+    public static void Aufgabe6Test()
+    {
+        StringArrayBoolTest[] AufgabeSechsTests = {
+                   new StringArrayBoolTest { input = new string[]{"Deine Mom", "Dein Opa", "Deine Oma", "Deine Schwester", "Dein Urgroßvater" }, output = Aufgaben.Aufgabe6(new string[]{"Deine Mom", "Dein Opa", "Deine Oma", "Deine Schwester", "Dein Urgroßvater" }), expectedOutput = false,hidden = true },
+                   new StringArrayBoolTest { input = new string[]{"Naturensohn", "Hungerlohn", "Downsyndrom", "Mindestlohn", "Wer bist du denn schon" },output = Aufgaben.Aufgabe6(new string[]{"Naturensohn", "Hungerlohn", "Downsyndrom", "Mindestlohn", "Wer bist du denn schon" }), expectedOutput = false },
+                   new StringArrayBoolTest { input = new string[]{"Never","Gonna","Give","You","Up","Never","Gonna","Let","You","Down","Hurensohn"}, output = Aufgaben.Aufgabe6(new string[]{"Never","Gonna","Give","You","Up","Never","Gonna","Let","You","Down","Hurensohn"}), expectedOutput = true },
+                   new StringArrayBoolTest { input = new string[]{"Photoshop","war","ein","Joke","ich","war","da","nicht","mal","ein","Hurensohn"}, output = Aufgaben.Aufgabe6(new string[]{"Never","Gonna","Give","You","Up","Never","Gonna","Let","You","Down","Hurensohn"}), expectedOutput = true }
+
+               };
+        foreach (var item in AufgabeSechsTests)
+        {
+            Tester.TestStringArrayBool(item);
+        }
+    }
+    public static void Aufgabe7Test()
+    {
+        IntInIntOutTest[] AufgabeSechsTests =
+        {
+            new IntInIntOutTest {input = 999, output = Aufgaben.Aufgabe7(999), expectedOutput = 27, hidden = false},
+            new IntInIntOutTest {input = 123, output = Aufgaben.Aufgabe7(123), expectedOutput = 6, hidden = false},
+            new IntInIntOutTest {input = 420, output = Aufgaben.Aufgabe7(420), expectedOutput = 6, hidden = false},
+            new IntInIntOutTest {input = 69, output = Aufgaben.Aufgabe7(69), expectedOutput = 15, hidden = true},
+            new IntInIntOutTest {input = 666, output = Aufgaben.Aufgabe7(666), expectedOutput = 18, hidden = true},
+        };
+    }
+    
+
+}
